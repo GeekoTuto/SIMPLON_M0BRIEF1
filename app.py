2c1bbdbac55bdccdda839ffddf0a8c0b24baee87
fredericfoudrain <geeko.tuto@gmail.com>
2026-03-31 15:16:10 +0400

    feat(log): log pour streamlit

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import requests
 import streamlit as st
-
+from loguru import logger
+from pathlib import Path
 
 API_URL = "http://127.0.0.1:8001/analyse_sentiment/"
 
@@ -12,6 +13,14 @@ def get_sentiment_text(compound_score: float) -> str:
 		return "Sentiment global : Négatif 🙁"
 	return "Sentiment global : Neutre 😐"
 
+#@st.cache_resource
+#def setup_logger():
+#    logger.add("logs/sentiment_streamlit.log", rotation="500 MB", level="INFO")
+
+#setup_logger()
+
+logger.remove()
+logger.add("logs/sentiment_streamlit.log", rotation="500 MB", level="INFO")
 
 st.set_page_config(page_title="Analyse de sentiment", layout="centered")
 
@@ -27,6 +36,7 @@ if st.button("Analyser", type="secondary"):
 	else:
 		try:
 			response = requests.post(api_url, json={"text": text})
+			logger.info(f"Appel API : URL={api_url}, texte='{text}'")
 			response.raise_for_status()
 			result = response.json()
 			sentiment_text = get_sentiment_text(result["compound"])
@@ -34,10 +44,15 @@ if st.button("Analyser", type="secondary"):
 			st.success(f"Sentiment : {sentiment_text}")
 			st.metric("Score global", f"{result['compound']:.4f}")
 			st.subheader("Details")
+			logger.info(f"Résultat API : {result}")
+			logger.info(f"Sentiment global : {sentiment_text}")
 			st.json(result)
 		except requests.exceptions.ConnectionError:
+			logger.error(f"Connexion impossible a l'API : URL={api_url}")
 			st.error("Connexion impossible a l'API")
 		except requests.exceptions.HTTPError as exc:
+			logger.error(f"Erreur HTTP : {exc}")
 			st.error(f"Erreur HTTP : {exc}")
 		except requests.exceptions.RequestException as exc:
+			logger.error(f"Erreur lors de l'appel API : {exc}") 
 			st.error(f"Erreur lors de l'appel API : {exc}")
